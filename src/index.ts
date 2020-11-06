ac9ffc47e03ef763f434d10648f10cd1eb9edd59
PierpaoloIannone <piannone@byte-code.com>
2020-11-06 09:13:21 +0100

    fix: create interface for sdk

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,46 @@
 import { createRestApi } from './api';
 import { connect } from './channel';
 import { SchemaField } from './schema_field';
+import {
+  API,
+  APIContentMessage,
+  APIDefinitionMessage,
+  APIPublishingChannelMessage,
+  APIPublishMessage,
+  APIRepositoryMessage,
+} from './types';
 import UI from './ui';
+
+export interface SDK {
+  api: {
+    content: API<APIContentMessage>;
+    publishingChannel: API<APIPublishingChannelMessage>;
+    repository: API<APIRepositoryMessage>;
+    definition: API<APIDefinitionMessage>;
+    publish: API<APIPublishMessage>;
+  };
+  ui: UI;
+  field: SchemaField;
+}
 
 function initialize(currentWindow: Window) {
   const connectDeferred = createDeferred();
 
   connect(currentWindow, (...args) => connectDeferred.resolve(args));
 
-  return (callback: (sdk: any) => any) => {
+  return (callback: (sdk: SDK) => any) => {
     connectDeferred.promise.then(([channel, initMessage]) => {
       const restApi = createRestApi(channel);
       const ui = new UI(channel, currentWindow);
       const field = new SchemaField(channel, initMessage.field);
 
-      const api = {
+      const sdk = {
         api: { ...restApi },
         ui,
         field,
       };
 
-      callback(api);
+      callback(sdk);
     });
   };
 }
